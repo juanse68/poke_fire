da9198492c2ea05b4a04e7846447ae0cdba11918
juanse68 <163223814+juanse68@users.noreply.github.com>
2025-05-22 19:59:17 +0000

    config firebase

--- proyecto/src/firebaseConfig.js
+++ proyecto/src/firebaseConfig.js
@@ -3,12 +3,12 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-apiKey: "aqui su codigo",
-authDomain: "aqui su codigo",
-projectId: "aqui su codigo",
-storageBucket: "aqui su codigo",
-messagingSenderId: "aqui su codigo",
-appId: "aqui su codigo"
+apiKey: "AIzaSyBWI-OQAP91N3VFU1M8gtGzOj8bqrjW-fs",
+    authDomain: "pokefire-f5648.firebaseapp.com",
+    projectId: "pokefire-f5648",
+    storageBucket: "pokefire-f5648.firebasestorage.app",
+    messagingSenderId: "556818644849",
+    appId: "1:556818644849:web:77bdf63952541f2c86c0d0",
 };
 
 const app = initializeApp(firebaseConfig);
